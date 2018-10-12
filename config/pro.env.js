a99b3e950ab0bb2f9d499c009fa3f85ccc446983
zyn <18240364@qq.com>
2018-10-12 11:34:38 +0800

    环境设置

--- config/pro.env.js
+++ config/pro.env.js
@@ -1,6 +1,7 @@
+const hostUrl = '"http://172.27.4.71:8088"';
 // 生产环境
 module.exports = {
   NODE_ENV: '"production"',
-  hostUrl: '',
+  hostUrl: hostUrl,
   urlPrefix: '"/api"'
 };
--- config/uat.env.js
+++ config/uat.env.js
@@ -1,6 +1,7 @@
+const hostUrl = '"http://172.27.4.71:8088"';
 // 测试环境
 module.exports = {
   NODE_ENV: '"test"',
-  hostUrl: '',
+  hostUrl: hostUrl,
   urlPrefix: '"/api"'
 };
